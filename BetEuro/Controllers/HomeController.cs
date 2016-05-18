a415980c937d60ceae8c3ac5ae319e9c4c07063b
ivaangg <ivaan84@gmail.com>
2016-05-18 13:44:50 +0200

    Before Fotmob

--- BetEuro/Controllers/HomeController.cs
+++ BetEuro/Controllers/HomeController.cs
@@ -1,17 +1,33 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
 using System.Linq;
+using System.Threading.Tasks;
+using System.Net;
 using System.Web;
 using System.Web.Mvc;
+using BetEuro;
 
 namespace BetEuro.Controllers
 {
     public class HomeController : Controller
     {
-        public ActionResult Index()
+        private BEEntities db = new BEEntities();
+
+        public async Task<ActionResult> Index()
         {
-            return View();
+            var matches = db.Matches.Include(m => m.AwayTeam).Include(m => m.HomeTeam).Include(m => m.Score);
+            return View(await matches.ToListAsync());
         }
 
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
     }
 }
