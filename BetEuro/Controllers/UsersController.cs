07d6567e83b6934a2bc732a14070e07ccd8d555d
ivaangg <ivaan84@gmail.com>
2016-06-17 12:03:06 +0200

    Stats, plus reg close

--- BetEuro/Controllers/UsersController.cs
+++ BetEuro/Controllers/UsersController.cs
@@ -89,28 +89,6 @@ namespace BetEuro.Controllers
         {
             if (ModelState.IsValid)
             {
-                if (user.isActive && !db.Users.Single(p => p.Id == user.Id).isActive && user.Piwo)
-                {
-                    string message =
-                        "Witam," + Environment.NewLine +
-                        "Opcja PIWO pozwala na grę o nagrodę wpłacając 10 PLN na konto podane poniżej przeznaczasz 8 PLN do puli nagród oraz 2 PLN na utrzymanie serwisu. Nagrodę otrzyma osoba, która uzbiera najwięcej punktów po zakończeniu mistrzostw. O nagrodę grają tylko osoby, które wpłaciły 10 PLN. Poniżej dane do przelewu:" + Environment.NewLine +
-                        "Dane do przelewu:" + Environment.NewLine +
-                        "Łukasz Iwankow" + Environment.NewLine +
-                        "50 1020 5558 1111 1440 2950 0042" + Environment.NewLine +
-                        "Tytuł wiadomości: Twój username lub mail." + Environment.NewLine + Environment.NewLine +
-                        "Mail wygenerowany automatycznie proszę na niego nie odpowiadać. W przypadku potrzeby kontaktu proszę pisać na ivaan84@gmail.com";
-                    await SendMail(db.Users.Single(p => p.Id == user.Id).Email, "PIWO - aktywacja", message);
-                }
-
-                if (user.Paid && !db.Users.Single(p => p.Id == user.Id).Paid)
-                {
-                    string message =
-                        "Witam," + Environment.NewLine +
-                        "Opcja PIWO została aktywowana." + Environment.NewLine + Environment.NewLine +
-                        "Mail wygenerowany automatycznie proszę na niego nie odpowiadać. W przypadku potrzeby kontaktu proszę pisać na ivaan84@gmail.com";
-                    await SendMail(db.Users.Single(p => p.Id == user.Id).Email, "PIWO - usługa aktywowana", message);
-                }
-
                 db.Users.Single(p => p.Id == user.Id).isActive = user.isActive;
                 db.Users.Single(p => p.Id == user.Id).Piwo = user.Piwo;
                 db.Users.Single(p => p.Id == user.Id).Paid = user.Paid;
